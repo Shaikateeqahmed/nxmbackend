0dc3a77eb4289280a5204d6eb39d9f7f95f44c05
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-01-14 00:07:31 +0530

    deploy

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,8 +5,8 @@ const token = req.headers.authorization;
 //console.log(token);
    jwt.verify(token,process.env.key,(err,decode)=>{
     console.log(decode);
-    // const userID=decode.userID;
-    // req.body.userID=userID;
+    const userID=decode.userID;
+    req.body.userID=userID;
     if(decode){
         next();
     }else{
