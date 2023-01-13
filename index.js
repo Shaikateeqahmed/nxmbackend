178cbce3390b9de8087d4866fd48beef413a370d
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-01-14 00:06:10 +0530

    deploy

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const cors = require("cors");
 const {user} = require("./routes/userroute.js");
 const {connection} = require("./config/db.js");
 const {note} = require("./routes/noteroute.js");
-//const {authenticate} = require("./middlewares/authenticate.js");
+const {authenticate} = require("./middlewares/authenticate.js");
 require('dotenv').config();
 const app = express();
-var corsOptions = {
-    origin: 'http://example.com"https://puce-zealous-caiman.cyclic.app/"',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-  console.log(cors);
-app.use(cors({origin:'*'}));
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+//app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use("/user",user);
 app.use(authenticate);
